6638682c5dc12b94a7f88aa98ad76cd641ac1dc3
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-14 11:17:51 +0530

    Project stage 2/4

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -7,11 +7,25 @@ third_month = 'Month 3: repaid 500'
 # write your code here
 print(loan_principal + "\n" + first_month + "\n" + second_month + "\n" + third_month + "\n" + final_output)
 """
+from math import ceil
+
 loan_principal = int(input("Enter the loan principal:\n"))
 choice = input("What do you want to calculate?\ntype \"m\" - for number of monthly payments,\ntype \"p\" - for the monthly payment:\n")
 if choice == "m":
     monthly_payment = int(input("Enter the monthly payment:\n"))
-    print("It will take {} months to replay the loan".format(loan_principal // monthly_payment))
+    print()
+    period = ceil(loan_principal / monthly_payment)
+    if period == 1:
+        print("It will take {} month to replay the loan".format(ceil(loan_principal / monthly_payment)))
+    else:
+        print("It will take {} months to replay the loan".format(ceil(loan_principal / monthly_payment)))
 elif choice == "p":
     no_of_months = int(input("Enter the number of months:\n"))
-
+    if loan_principal // no_of_months == ceil(loan_principal / no_of_months):
+        print()
+        print("Your monthly payment = {}".format(loan_principal // no_of_months))
+    else:
+        print()
+        payment = ceil(loan_principal / no_of_months)
+        last_payment = loan_principal - (no_of_months - 1) * payment
+        print("Your monthly payment = {} and the last payment = {}".format(payment, last_payment))
